493d5b8353658b608c0b38968e6d76d676e59cd6
liwuv587 <99467354+liwuv587@users.noreply.github.com>
2024-03-20 13:30:05 +0000

    feat cal

--- pages/cost-calculator.vue
+++ pages/cost-calculator.vue
@@ -1,26 +1,53 @@
 <template>
-    <div h-full text-center flex select-none all:transition-400 clear-inherit>
-        <el-affix :offset="120">
-    <el-button type="primary">Offset top 120px</el-button>
-  </el-affix>
-        <div ma>
-            <el-container>
-                <el-main>
-
-
-                    <div class="text-red-500">This should be red.</div>
-
-                    <div class="p-4 text-lg text-blue-500 hover:text-blue-700">
-                        Hello, UnoCSS!
-                    </div>
-                    <div>
-                        <el-button>dd</el-button>
-                    </div>
-                </el-main>
-            </el-container>
-        </div>
-    </div>
-</template>
-
-
-<style></style>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="Approved by">
+        <el-input v-model="formInline.user" placeholder="Approved by" clearable />
+      </el-form-item>
+      <el-form-item label="Activity zone">
+        <el-select
+          v-model="formInline.region"
+          placeholder="Activity zone"
+          clearable
+        >
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Activity time">
+        <el-date-picker
+          v-model="formInline.date"
+          type="date"
+          placeholder="Pick a date"
+          clearable
+        />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Query</el-button>
+      </el-form-item>
+    </el-form>
+  </template>
+  
+  <script lang="ts" setup>
+  import { reactive } from 'vue'
+  
+  const formInline = reactive({
+    user: '',
+    region: '',
+    date: '',
+  })
+  
+  const onSubmit = () => {
+    console.log('submit!')
+  }
+  </script>
+  
+  <style>
+  .demo-form-inline .el-input {
+    --el-input-width: 220px;
+  }
+  
+  .demo-form-inline .el-select {
+    --el-select-width: 220px;
+  }
+  </style>
+  
