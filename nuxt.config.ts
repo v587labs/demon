30d26e81fea77fb89bbe5444ef2158a09c06c8c7
liwuv587 <99467354+liwuv587@users.noreply.github.com>
2024-03-20 03:08:56 +0000

    feat add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@element-plus/nuxt'],
-  devtools: { enabled: true }, 
+  devtools: { enabled: true },
+  ssr: false,
+  modules: ['@element-plus/nuxt', '@pinia/nuxt'],
 })
