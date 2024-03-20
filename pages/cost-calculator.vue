ba80512c676df0af9148c1c3b451aaa058b80fdf
liwuv587 <99467354+liwuv587@users.noreply.github.com>
2024-03-20 14:55:38 +0000

    feat cost

--- pages/cost-calculator.vue
+++ pages/cost-calculator.vue
@@ -1,56 +1,121 @@
 <template>
     <el-form :inline="true" :model="formInline" class="demo-form-inline">
         <el-form-item label="开仓价格">
-            <el-input-number v-model="formInline.open" type="number" placeholder="开仓价格" clearable />
+            <el-input-number v-model="formInline.open" type="number" :step="10" placeholder="开仓价格" clearable />
         </el-form-item>
 
 
         <el-form-item label="成本">
-            <el-input-number v-model="formInline.cost"
-            
-    :formatter="(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
-    :parser="(value) => value.replace(/\$\s?|(,*)/g, '')"
-            type="number" placeholder="成本" clearable />
+            <el-input-number v-model="formInline.cost" type="number" placeholder="成本" :step="10" clearable />
         </el-form-item>
 
         <el-form-item label="倍率">
-            <el-input-number v-model="formInline.rate" type="number" placeholder="倍率" clearable />
+            <el-input-number v-model="formInline.rate" :min="1" :max="100" type="number" placeholder="倍率" clearable />
         </el-form-item>
 
         <el-form-item label="加仓率">
-            <el-input-number v-model="formInline.addRate" type="number" placeholder="倍率" clearable />
+            <el-input-number v-model="formInline.addRate" :min="-1" :max="1" :step="0.01" type="number" placeholder="倍率"
+                clearable />
         </el-form-item>
 
         <el-form-item label="加仓成本">
-            <el-input-number v-model="formInline.addCost" type="number" placeholder="倍率" clearable />
+            <el-input-number v-model="formInline.addCost" :step="10" type="number" placeholder="倍率" clearable />
         </el-form-item>
 
         <el-form-item label="加仓次数">
             <el-input-number v-model="formInline.addNum" type="number" placeholder="倍率" clearable />
         </el-form-item>
-
+        <div></div>
         <el-form-item>
             <el-button type="primary" @click="onSubmit">计算</el-button>
         </el-form-item>
     </el-form>
+
+    <el-card  v-for="(item, index) in listRef" mb-10px style="max-width: 480px">
+        <template #header>
+            <div class="card-header">
+                <span>{{index==0?'成本':'加仓 '+index}}</span>
+            </div>
+        </template>
+        <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+        <template #footer>
+            收益：  <el-text v-if="item.profit<0" class="mx-1" type="danger">{{item.profit}}  (-{{item.profitRate}} %)</el-text>
+            <el-text v-else-if="item.profit==0" class="mx-1"  >{{item.profit}}</el-text>
+            <el-text v-else class="mx-1"  >{{item.profit}} ({{item.profitRate}} %)</el-text>
+ 
+        </template>
+    </el-card>
 </template>
 
 <script lang="ts" setup>
+
 import { reactive } from 'vue'
 
-const formInline = reactive({
-    open: '',
-    cost: '',
-    rate: 5,
-    addRate: 0.01,
-    addCost: '',
-    addNum: 10,
-
+useHead({
+    title: '合约成本计算器',
+    //   meta: [
+    //     { name: 'description', content: 'My amazing site.' }
+    //   ],
+    //   bodyAttrs: {
+    //     class: 'test'
+    //   },
+    //   script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
 })
 
+const formInline = reactive({
+    open: 1,
+    cost: 10,
+    rate: 5,
+    addRate: -0.01,
+    addCost: 10,
+    addNum: 10,
+
+});
+
+const listRef = ref([]);
+
 const onSubmit = () => {
-    console.log('submit!')
+
+    let list = [];
+
+    for (let i = 0; i < formInline.addNum + 1; i++) {
+        console.log(i)
+
+        if (i == 0) {
+            list.push({
+                "price": formInline.open,
+                "num": formInline.cost / formInline.open,
+                "cost": formInline.cost,
+                "profit": 0,
+                "profitRate": 0,
+            })
+        } else {
+            let last = list[i - 1];
+            let price = last.price * (1 + formInline.addRate * i);
+            let cost = formInline.addCost;
+            let num = cost / price;
+            let nowCost = last.cost + cost;
+            let nowNum = num + last.num;
+            let nowPrice = nowCost / nowNum;
+            let nowProfit = (nowPrice - last.price) * formInline.rate * nowCost;
+
+            list.push({
+                "price": nowPrice,
+                "num": nowNum,
+                "cost": nowCost,
+                "profit": nowProfit,
+                "profitRate": (1-(nowCost+nowProfit)/nowCost)*100,
+            })
+        }
+
+
+
+    }
+
+    listRef.value = list;
+    console.log(list);
 }
+
 </script>
 
 <style>
