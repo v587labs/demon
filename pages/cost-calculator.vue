d43129469efd8ee65f0cc38e327c471e9bc36169
liwuv587 <99467354+liwuv587@users.noreply.github.com>
2024-03-20 13:48:21 +0000

    xost

--- pages/cost-calculator.vue
+++ pages/cost-calculator.vue
@@ -1,20 +1,32 @@
 <template>
     <el-form :inline="true" :model="formInline" class="demo-form-inline">
         <el-form-item label="开仓价格">
-            <el-input-number  v-model="formInline.open" type="number" placeholder="开仓价格" clearable />
+            <el-input-number v-model="formInline.open" type="number" placeholder="开仓价格" clearable />
         </el-form-item>
 
 
         <el-form-item label="成本">
-            <el-input-number  v-model="formInline.cost" type="number" placeholder="成本" clearable />
+            <el-input-number v-model="formInline.cost"
+            
+    :formatter="(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')"
+    :parser="(value) => value.replace(/\$\s?|(,*)/g, '')"
+            type="number" placeholder="成本" clearable />
         </el-form-item>
 
         <el-form-item label="倍率">
-            <el-input-number  v-model="formInline.rate" type="number" placeholder="倍率" clearable />
+            <el-input-number v-model="formInline.rate" type="number" placeholder="倍率" clearable />
         </el-form-item>
 
-        <el-form-item label="倍率">
-            <el-input-number  v-model="formInline.cost" type="number" placeholder="倍率" clearable />
+        <el-form-item label="加仓率">
+            <el-input-number v-model="formInline.addRate" type="number" placeholder="倍率" clearable />
+        </el-form-item>
+
+        <el-form-item label="加仓成本">
+            <el-input-number v-model="formInline.addCost" type="number" placeholder="倍率" clearable />
+        </el-form-item>
+
+        <el-form-item label="加仓次数">
+            <el-input-number v-model="formInline.addNum" type="number" placeholder="倍率" clearable />
         </el-form-item>
 
         <el-form-item>
@@ -30,8 +42,9 @@ const formInline = reactive({
     open: '',
     cost: '',
     rate: 5,
-    addRate:0.01,
-    addCost: 100,
+    addRate: 0.01,
+    addCost: '',
+    addNum: 10,
 
 })
 
